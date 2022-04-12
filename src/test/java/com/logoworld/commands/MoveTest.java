dda20c9701b7ef1a926cef079d0802d9c9cf2788
egorkuzn <egorkuzn2002@mail.ru>
2022-04-12 16:05:46 +0700

    starting of move test writing

--- src/test/java/com/logoworld/commands/MoveTest.java
+++ src/test/java/com/logoworld/commands/MoveTest.java
@@ -1,16 +1,46 @@
 package com.logoworld.commands;
 
+import com.logoworld.exceptions.BadParam;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveTest {
+    Move obj;
+    String param;
 
-    @Test
-    void getParam() {
+    @BeforeEach
+    void setUp(){
+        obj = new Move();
     }
 
     @Test
+    @DisplayName("Testing of getParam method of MOVE cmd")
+    void getParam() {
+        param = null;
+        try{
+            obj.getParam(param);
+            fail();
+        } catch (BadParam e) {
+            assertNotNull(e.getMessage());
+        }
+
+        param = "90 9 9";
+        try{
+            obj.getParam(param);
+            fail();
+        } catch (BadParam e) {
+            assertNotNull(e.getMessage());
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("Testing of action(Field, Robot, String)")
     void action() {
+
     }
 }
